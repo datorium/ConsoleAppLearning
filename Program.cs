c5087d6350fee30dae8dd38a7534ae3ceb4ca00a
Datorium <hello@datorium.lv>
2020-06-13 17:44:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person = new Human();
+            person.Name = "Jenifer";
+            person.Surname = "Lopez";
+            person.Age = 25;
+            person.EyeColor = "Brown";
+            person.Introduce();
+
+            person.Name = "Anna";
+            person.Introduce();
+        }
+    }
+}
